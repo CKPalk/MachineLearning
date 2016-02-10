6283ebce4448ceeb3a7e51ba798e05e8fb3b9526
Cameron Palk <cpalk@d31-244.uoregon.edu>
2016-02-10 15:57:31 -0800

    ,

--- temp.py
+++ temp.py
@@ -1,4 +1,5 @@
 
+import sys
 import itertools
 
 
@@ -7,41 +8,50 @@ min_conf = 0.75
 
 
 transactions = [ 
-    set( list( 'ABCDEF' ) ),
-    set( list( 'BHSCFT' ) ),
-    set( list( 'AUOFWD' ) ),
-    set( list( 'OAECFG' ) ),
-    set( list( 'XACDEF' ) )
+	set( list( 'ABCDEF' ) ),
+	set( list( 'BHSCFT' ) ),
+	set( list( 'AUOFWD' ) ),
+	set( list( 'OAECFG' ) ),
+	set( list( 'XACDEF' ) )
 ]
 
 
 def transactionsWith( test_set, transactions ):
-    return len( list( filter( lambda x: x >= test_set, transactions ) ) )
+	return len( list( filter( lambda x: x >= test_set, transactions ) ) )
 
 def supportFor( test_set, transactions ):
-    return transactionsWith( test_set, transactions ) / len( transactions )
+	return transactionsWith( test_set, transactions ) / len( transactions )
 
-def printConfidencesFor( test_set, transactions ):
-    for elem in test_set:
-        new_set = test_set - set(elem)
-        confidence = transactionsWith( test_set, transactions ) / transactionsWith( new_set, transactions )
-        print( "\t", new_set, " + ", elem, ":", confidence )
+def printConfidencesFor( test_set, transactions, support, stream_out ):
+	for elem in test_set:
+		new_set = test_set - set(elem)
+		confidence = transactionsWith( test_set, transactions ) / transactionsWith( new_set, transactions )
+		if confidence < min_conf:
+			continue
+		res = ""
+		for item in new_set:
+			if res is not "":
+				res += " ^ "
+			res += "buys(X,{})".format( item )
+		res += " => buys(X,{})[{}%, {}%]\n".format( elem, int( support * 100 ), int( confidence * 100 ) )
+		stream_out.write( res )
 
 def subsetsOfLength( length ):
-    possible = set( list( 'ABCDEFHSTOUFWDOAECFGX' ) )
-    return set( itertools.combinations( possible, length ) )
+	possible = set( list( 'ABCDEFHSTOUFWDOAECFGX' ) )
+	return set( itertools.combinations( possible, length ) )
 
 
-def main():
-    print( transactions )
-    for length in range( 1, 5 ):
-        print( "\n--- LENGTH", length, "---\n" )
-        for s in subsetsOfLength( length ):
-            sup = supportFor( set(s), transactions )
-            if sup >= min_sup:
-                print( set(s), ":", sup )
-                printConfidencesFor( set(s), transactions )
+def main( argv ):
+	print( transactions )
+	with open( argv[0], 'w+' ) as stream_out:
+		for length in range( 2, 5 ):
+			stream_out.write( "\n--- Associations of length {} ---\n".format( length ) )
+			for s in subsetsOfLength( length ):
+				sup = supportFor( set(s), transactions )
+				if sup >= min_sup:
+					#print( set(s), ":", sup )
+					printConfidencesFor( set(s), transactions, sup, stream_out )
 
 
 if __name__=='__main__':
-	main()
+	main(sys.argv[1:])
